1143e9b0313255ecaa13da2d7103acc732cae0bc
João Vitor Condidorio <joaovitorluz010@gmail.com>
2024-07-25 17:44:51 -0300

    Tentando commitar na nova Branch

---
+++ cypress/e2e/mainTeste.spec.cy.js
@@ -0,0 +1,11 @@
+describe('Testando funcionalidade de comentários em uma rede social', () => {
+    it('Comentar em um publica e depois apagar o comentário', () => {
+      cy.visit('localhost:5173/')
+      cy.get('[data-qa="profile-box"]')
+      cy.get('[data-qa="comment-input"]').first().type('LEGAL GOSTEI DA PÚBLICAÇÃO - TESTE')
+      cy.get('[data-qa="publish-button"]').eq(0).click()
+      cy.get('[data-qa="comment-text"]').eq(1).contains('LEGAL GOSTEI DA PÚBLICAÇÃO - TESTE')
+      cy.get('[data-qa="delete-button"]').eq(1).click()
+    })
+  })
+  
